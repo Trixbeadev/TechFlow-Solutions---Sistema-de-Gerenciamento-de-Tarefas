a33e706eb23daff2d9757630a597f34ee680aa73
Trixbeadev <beatrizhelenafranca@gmail.com>
2025-11-08 12:47:06 -0300

    feat: adicionar rota para listar tarefas

--- src/app.py
+++ src/app.py
@@ -1,2 +1,3 @@
 from flask import Flask\napp = Flask(__name__)\n\n@app.route('/')\ndef index():\n    return 'TechFlow funcionando'WPS Cloud Files'/
 # Rota Flask para criar tarefas
+# Rota Flask para listar tarefas
